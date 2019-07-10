978f282ec1846a3ae0b5f3244e7c28d550edd8eb
Mikhail <43968821+MihellMelnik@users.noreply.github.com>
2019-07-10 13:45:31 +0300

    Add files via upload

---
+++ cat.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cstdlib>
+int main ()
+{
+  printf ("\n  _"); 
+  printf ("\n ( \\");
+  printf ("\n  ) )  ");
+  printf ("\n ( (  .-\"\"-.  A.-.A  ");
+  printf ("\n \\ \\/       \\/ , , \\");    
+  printf ("\n  \\    \\    =;  t  /=");
+  printf ("\n   \\    |\"\".  ',--'"); 
+  printf ("\n    /  //  | ||");
+  printf ("\n   /_ ,))  |_,))");
+  printf ("\n\n\n");
+  system("pause");
+  return 0;
+}
+
+
+   
+   
---
+++ elefant.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main ()
+{
+  printf ("\n          _     _ "); 
+  printf ("\n         / \\```/ \\");
+  printf ("\n   ,----(    ..   )");
+  printf ("\n  /      \\_     _/");
+  printf ("\n /|        (\\  |(");
+  printf ("\n^ \\   /__\\  /\\ |");
+  printf ("\n   |__|  |__|-\" ");
+ 
+  return 0;
+}
+
+   
+   
